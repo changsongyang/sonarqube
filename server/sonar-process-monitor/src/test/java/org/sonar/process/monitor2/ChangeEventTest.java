116795dc51421de8897559d041fc246ec7609aa3
Eric Hartmann <hartmann.eric@gmail.com>
2017-03-03 17:58:43 +0100

    work on unit tests (wip)

---
+++ server/sonar-process-monitor/src/test/java/org/sonar/process/monitor2/ChangeEventTest.java
@@ -0,0 +1,30 @@
+package org.sonar.process.monitor2;
+
+import org.junit.Test;
+import org.sonar.process.ProcessId;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ChangeEventTest {
+  @Test
+  public void equals_and_hashcode() {
+    ChangeEvent changeEvent = new ChangeEvent(ProcessId.APP, ChangeEventType.STARTED);
+    for (ChangeEventType type : ChangeEventType.values()) {
+      ChangeEvent temp = new ChangeEvent(ProcessId.APP, type);
+      assertThat(changeEvent.equals(temp)).isEqualTo(type.equals(ChangeEventType.STARTED));
+    }
+    for (ProcessId processId : ProcessId.values()) {
+      ChangeEvent temp = new ChangeEvent(processId, ChangeEventType.STARTED);
+      assertThat(changeEvent.equals(temp)).isEqualTo(processId.equals(ProcessId.APP));
+    }
+
+    assertThat(changeEvent.equals(changeEvent)).isTrue();
+    assertThat(changeEvent.equals("STARTED")).isFalse();
+    assertThat(changeEvent.toString())
+      .isEqualTo(
+        String.format("ChangeEvent{type=%s, processId=%s}",
+          changeEvent.getType(), changeEvent.getProcessId()
+        )
+      );
+  }
+}
